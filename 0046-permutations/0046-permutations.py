1ac02dae1afcd4cf5f371ec2441d4a97e8da05a0
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-10-13 16:36:33 +0530

    Time: 49 ms (82.81%), Space: 14.1 MB (23.39%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,26 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        def backtrack(counter, cur_comb):
+            nonlocal res
+            
+            if len(cur_comb) == len(nums):
+                res.append(cur_comb[:])
+                return 
+            
+            for key in counter:
+                if counter[key] > 0:
+                    cur_comb.append(key)
+                    counter[key] -= 1
+                    
+                    backtrack(counter, cur_comb)
+                    
+                    cur_comb.pop()
+                    counter[key] += 1
+                    
+        counter = Counter(nums)
+        cur_comb = []
+        backtrack(counter, cur_comb)
+        return res
+        
