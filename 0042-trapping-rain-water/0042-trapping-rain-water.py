2c40f29789435555bef904141aa52ac24752e674
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-10-13 11:54:14 +0530

    Time: 258 ms (50.61%), Space: 16 MB (46.89%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,31 +1,24 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        max_left = [0]*len(height)
-        max_till_now = height[0]
-        for i in range(1, len(height)):
-            max_left[i] = max_till_now
-            
-            if max_till_now < height[i]:
-                max_till_now = height[i]
+        if not height:
+            return 0 
         
-        max_right = [0]*len(height)
-        max_till_now = height[-1]
-        for i in range(len(height) - 2, -1, -1):
-            max_right[i] = max_till_now
-            
-            if max_till_now < height[i]:
-                max_till_now = height[i]
+        l, r = 0, len(height) - 1
+        leftMax = height[l]
+        rightMax = height[r]
+        res = 0
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l] 
                 
-        min_left_right = [0]*len(height)
-        for i in range(len(height)):
-            min_left_right[i] = min(max_left[i], max_right[i])
-            
-        
-        ans = 0
-        for i in range(len(height)):
-            if min_left_right[i] - height[i] > 0:
-                ans += (min_left_right[i] - height[i])
-        return ans
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+                
+        return res
         
                 
         
