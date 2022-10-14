928707bb6098965fac8b48eb6d91fb12d422e0d7
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-10-14 13:29:28 +0530

    Time: 52 ms (51.32%), Space: 13.9 MB (11.78%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        @cache
+        def climb(x):
+            if x < 0:
+                return 0 
+            
+            elif x == 0:
+                return 1
+            
+            total = climb(x - 1) + climb(x - 2)
+            return total
+        
+        return climb(n)
