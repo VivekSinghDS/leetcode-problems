4fb1671cd83368fd1add9c0999a2b7e8a5efae32
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-10-20 13:45:54 +0530

    Time: 20 ms (99.84%), Space: 13.9 MB (33.59%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -3,18 +3,19 @@ class Solution:
         res = []
         wordDict = set(wordDict)
         
-
+        @cache
         def backtrack(cur_comb, string):
             nonlocal res
+            cur_comb = list(cur_comb)
             if len(string) == 0:
                 res.append(" ".join(cur_comb))
                 return 
             
             for word in wordDict:
                 if string[:len(word)] == word:
-                    backtrack(cur_comb + [word], string[len(word):])
+                    backtrack(tuple(cur_comb + [word]), string[len(word):])
                     
-        backtrack([], s)
+        backtrack(tuple([]), s)
         return res
                     
             
