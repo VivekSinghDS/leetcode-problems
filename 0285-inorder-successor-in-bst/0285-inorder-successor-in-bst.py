b26998fde38343e5ef1638f196be1a6312d0b781
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-11-02 14:34:52 +0530

    Time: 87 ms (84.75%), Space: 18.8 MB (23.55%) - LeetHub

--- 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.py
+++ 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.py
@@ -7,6 +7,23 @@
 
 class Solution:
     def inorderSuccessor(self, root: TreeNode, p: TreeNode) -> Optional[TreeNode]:
+        '''
+        class Solution:
+    
+        def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'TreeNode':
+
+            successor = None
+
+            while root:
+
+                if p.val >= root.val:
+                    root = root.right
+                else:
+                    successor = root
+                    root = root.left
+
+            return successor
+        '''
         seen = False 
         stack = []
         cur = root
