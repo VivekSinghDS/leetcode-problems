9cde86acf45d02d9c8a70d92c6d6e5356b8aac66
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-11-02 14:25:25 +0530

    Time: 2551 ms (53.06%), Space: 13.8 MB (88.78%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -16,6 +16,42 @@ class Solution:
                 
     
         return(dp[n])
+    
+'''
+class Solution:
+    def numSquares(self, n: int) -> int:
+        if n == 1:
+            return 1
+        numbers = []
+        for i in range(1, int(n ** 0.5) + 1):
+            
+            if i ** 2 <= n:
+                numbers.append(i * i)
+        
+        
+        comb = []
+        res = float('inf')
+        
+        @lru_cache(maxsize = None)
+        def backtrack_reverse(remain, comb):
+            nonlocal res
+            if remain == 0:
+                # print(comb)
+                res = min(res, len(comb))
+                return 
+            
+            elif remain < 0 or len(comb) > res:
+                return  
+            
+            
+            comb = list(comb)
+            for number in numbers[::-1]:
+                backtrack_reverse(remain - number, tuple(comb + [number]))
+                
+        backtrack_reverse(n, tuple(comb))
+        return res
+
+'''
         
         
                 
