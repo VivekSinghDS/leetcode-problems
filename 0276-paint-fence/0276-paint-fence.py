bd05c14bd619cd6ef07aaa8e78e93309097bd703
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-11-02 13:02:19 +0530

    Time: 33 ms (91.53%), Space: 13.9 MB (21.56%) - LeetHub

--- 0276-paint-fence/0276-paint-fence.py
+++ 0276-paint-fence/0276-paint-fence.py
@@ -12,6 +12,18 @@ class Solution:
             return (solve(i - 1) + solve(i - 2))*(k - 1)
         
         return solve(n)
+    
+    '''
+    no_of_ways(i) = no_of_ways_similar(i) + no_of_ways_diff(i)
+    
+    for -> no_of_ways_diff(i - 1) = no_of_ways(i - 1) * (k - 1) ... The reason being we can color (k - 1) colors to the last fence
+    for -> no_of_ways_similar(i) = no_of_ways_diff(i - 1) = no_of_ways(i - 2)*(k - 1)
+    
+    adding both of them 
+    no_of_ways(i) = no_of_ways(i - 1) * (k - 1) + no_of_ways(i - 2)*(k - 2)
+    
+    
+    '''
             
             
         
