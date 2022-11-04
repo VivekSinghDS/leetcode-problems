9b966ffcffc644841266b8f399e2e10aa48dec98
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-11-04 10:58:53 +0530

    Time: 1561 ms (60.64%), Space: 17.3 MB (60.00%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -1,24 +1,48 @@
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
+        
+        '''
+        class Solution:
+            def maxProduct(self, words: List[str]) -> int:
+                n = len(words)
+                masks = [0] * n
+                lens = [0] * n
+                bit_number = lambda ch : ord(ch) - ord('a')
+
+                for i in range(n):
+                    bitmask = 0
+                    for ch in words[i]:
+                        # add bit number bit_number in bitmask
+                        bitmask |= 1 << bit_number(ch)
+                    masks[i] = bitmask
+                    lens[i] = len(words[i])
+
+                max_val = 0
+                for i in range(n):
+                    for j in range(i + 1, n):
+                        if masks[i] & masks[j] == 0:
+                            max_val = max(max_val, lens[i] * lens[j])
+                return max_val
+        '''
         def get_ord(ch):
             return ord(ch) - ord('a')
         
-        masks = [0]*len(words)
-        length = [0]*len(words)
+        # masks = [0]*len(words)
+        hashmap = defaultdict(int)
         
         for i in range(len(words)):
             bitmask = 0
             for ch in words[i]:
                 bitmask = bitmask | (1 << get_ord(ch))
                 
-            masks[i] = bitmask 
-            length[i] = len(words[i])
+            # masks[i] = bitmask 
+            hashmap[bitmask] = max(hashmap[bitmask], len(words[i]))
         
         res = 0
-        for i in range(len(words)):
-            for j in range(i):
-                if masks[i] & masks[j] == 0:
-                    res = max(res, length[i] * length[j])
+        for x in hashmap:
+            for y in hashmap:
+                if x & y == 0:
+                    res = max(res, hashmap[x] * hashmap[y])
                     
         return res
             
