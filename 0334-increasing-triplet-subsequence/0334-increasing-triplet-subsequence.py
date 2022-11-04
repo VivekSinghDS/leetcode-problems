11956d55f219423d2709fd071974b8a021224a81
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-11-04 15:11:31 +0530

    Time: 1677 ms (19.35%), Space: 29.1 MB (8.35%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,18 +1,25 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        first_num = float('inf')
-        second_num = float('inf')
         
-        for n in nums:
-            if n <= first_num:
-                first_num = n
-                
-            elif n <= second_num:
-                second_num = n 
+        sub = [nums[0]]
+        for i in range(1, len(nums)):
+            if sub[-1] < nums[i]:
+                sub.append(nums[i])
                 
             else:
-                return True 
-            
-        return False
-            
+                j = 0
+
+                while sub[j] < nums[i]:
+                    j += 1
+                    
+                sub[j] = nums[i]
+                
+        return len(sub) >= 3
+                
         
+                    
+        
+                
+                
+            
+         
