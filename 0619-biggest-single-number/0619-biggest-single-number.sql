41bce46dbc08e652360d2c5889b86b8f51041ff2
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-11-05 14:46:48 +0530

    Time: 421 ms (61.93%), Space: 0B (100.00%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -2,3 +2,8 @@
 select max(num) as num from 
 (select num from MyNumbers 
 group by num having COUNT(num) = 1) t
+
+/*
+select num from MyNumbers 
+group by num having COUNT(num) = 1 order by num desc limit 1
+*/
