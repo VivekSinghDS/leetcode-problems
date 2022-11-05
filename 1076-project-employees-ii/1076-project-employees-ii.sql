1540e8a43127b99d2fbfdafc9844a02e4bc52ffe
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-11-05 16:38:29 +0530

    Time: 1071 ms (55.70%), Space: 0B (100.00%) - LeetHub

--- 1076-project-employees-ii/1076-project-employees-ii.sql
+++ 1076-project-employees-ii/1076-project-employees-ii.sql
@@ -1,7 +1,15 @@
 # Write your MySQL query statement below
-select p.project_id from 
+# select p.project_id from 
+# (select project_id, count(employee_id) as "num_of_emp" from Project 
+# group by project_id ) p
+# where p.num_of_emp = (select max(x.num_of_emp) from 
+#                      (select project_id, count(employee_id) as "num_of_emp" from Project 
+#                         group by project_id ) x)
+
+with cte as 
 (select project_id, count(employee_id) as "num_of_emp" from Project 
-group by project_id ) p
-where p.num_of_emp = (select max(x.num_of_emp) from 
-                     (select project_id, count(employee_id) as "num_of_emp" from Project 
-group by project_id ) x)
+group by project_id)
+
+select project_id from 
+cte 
+where num_of_emp = (select max(num_of_emp) from cte)
