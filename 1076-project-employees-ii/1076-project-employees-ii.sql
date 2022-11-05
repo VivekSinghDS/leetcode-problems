6bb7338f8f5a3455747840fc33ef2f280e77daf4
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-11-05 16:36:37 +0530

    Time: 993 ms (71.55%), Space: 0B (100.00%) - LeetHub

---
+++ 1076-project-employees-ii/1076-project-employees-ii.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select p.project_id from 
+(select project_id, count(employee_id) as "num_of_emp" from Project 
+group by project_id ) p
+where p.num_of_emp = (select max(x.num_of_emp) from 
+                     (select project_id, count(employee_id) as "num_of_emp" from Project 
+group by project_id ) x)
