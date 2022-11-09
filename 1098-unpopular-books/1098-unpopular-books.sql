7cbb7f863cf6527567d348dc8cf797f6f9ef5007
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-11-09 16:56:31 +0530

    Time: 1039 ms (69.47%), Space: 0B (100.00%) - LeetHub

--- 1098-unpopular-books/1098-unpopular-books.sql
+++ 1098-unpopular-books/1098-unpopular-books.sql
@@ -1,7 +1,6 @@
-# Write your MySQL query statement below
 select b.book_id, b.name 
 from Books b LEFT JOIN Orders o 
 ON b.book_id = o.book_id 
-WHERE b.available_from <= date_sub("2019-06-23", INTERVAL 1 month)
-group by book_id
-having SUM(case when o.dispatch_date >= date_sub("2019-06-23", INTERVAL 1 Year) then o.quantity else 0 end) < 10
+WHERE b.available_from <= "2019-05-23"
+group by book_id 
+having SUM(case when o.dispatch_date >= "2018-06-23" then o.quantity else 0 end) < 10
