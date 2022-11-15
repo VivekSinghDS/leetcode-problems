23a82d924ae0856942b9c6f7f0a872a18a50c548
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-11-15 14:26:52 +0530

    Time: 45 ms (77.22%), Space: 13.9 MB (24.08%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,24 +1,49 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        stack_s = []
-        stack_t = []
-        for char in s:
-            if char == "#":
-                if stack_s:
-                    stack_s.pop()
-                
-            else:
-                stack_s.append(char)
-                
+        i = len(s) - 1
+        j = len(t) - 1
         
-        for char in t:
-            if char == "#":
-                if stack_t:
-                    stack_t.pop()
-                
-            else:
-                stack_t.append(char)
-                
-        return stack_s == stack_t
+        skip_s = 0
+        skip_t = 0 
+        
+        while i >= 0 or j >= 0:
+            while i >= 0:
+                if s[i] == "#":
+                    skip_s += 1
+                    i -= 1
+                    
+                elif skip_s > 0:
+                    skip_s -= 1
+                    i -= 1
+                    
+                else:
+                    break
+                    
+            while j >= 0:
+                if t[j] == "#":
+                    skip_t += 1
+                    j -= 1
+                    
+                elif skip_t > 0:
+                    skip_t -= 1
+                    j -= 1
+                    
+                else:
+                    break
+                    
+            if i >= 0 and j >= 0 and s[i] != t[j]:
+                return False 
             
+            if (i >= 0) != (j >= 0):
+                return False
+            
+            i -= 1
+            j -= 1
+        return True
+                    
+                
+                    
+                
+                    
+                
         
