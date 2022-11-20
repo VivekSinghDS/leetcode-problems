2d6b31de4e6cc6386422c13536b227d69f7c0a6c
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-11-20 10:21:09 +0530

    Time: 184 ms (79.46%), Space: 18.9 MB (5.35%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,26 +1,99 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        ans = 0 
-        m, n = len(grid), len(grid[0])
-        def dfs(r, c):        
-            visited.add((r, c))
-            area = 1
+        ans = 0
+        
+        def dfs(r, c):
+            if (r < 0 or c < 0 or r >= len(grid) or c >=len(grid[0])
+                or (r, c) in seen or grid[r][c] == 0):
+                return 0
             
-            for row, col in [(r + 1, c), (r - 1, c),
-                            (r, c + 1), (r, c - 1)]:
-                if 0 <= row < m and 0 <= col < n and grid[row][col] == 1 and (row, col) not in visited:
-                    area += dfs(row, col)
+            area = 1
+            paths = [(r + 1, c), 
+                    (r - 1, c), 
+                    (r, c + 1), 
+                    (r, c - 1)]
+            
+            seen.add((r, c))
+            for row, col in paths:
+                area += dfs(row, col)
                 
             return area
         
-        visited = set()
-        for r in range(m):
-            for c in range(n):
+        seen = set()
+        max_area = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
                 if grid[r][c] == 1:
-                    cur_area = dfs(r, c)
-                    ans = max(ans, cur_area)
+                    max_area = max(max_area, dfs(r, c))
                     
-        return ans
+        return max_area
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    
+'''
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        def dfs(r, c, border):
+            visited.add((r, c))
+            paths = [(r + 1, c), (r - 1, c),
+                        (r, c + 1), (r, c - 1)]
+            if border:
+                grid[r][c] = 0
+
+                for row, col in paths:
+                    if (0 <= row < len(grid) and 0 <= col < len(grid[0]) 
+                        and grid[row][col] == 1 and (row, col) not in visited):
+                        dfs(row, col, border)
+                        
+                return grid[r][c]
+                        
+            elif not border:
+                cur_area_count = 1
+                
+                for row, col in paths:
+                    if (0 <= row < len(grid) and 0 <= col < len(grid[0]) 
+                        and grid[row][col] == 1 and (row, col) not in visited):
+                        cur_area_count += dfs(row, col, border)
+                        
+                return cur_area_count
+
+        ROW = len(grid)
+        COL = len(grid[0])
+        visited = set()
+        max_area = 0
+        for r in range(ROW):
+            for c in range(COL):
+                if (r in [0, ROW - 1] or c in [0, COL - 1]) and grid[r][c] == 1:
+                    dfs(r, c, True)
+                    
+        for r in range(ROW):
+            for c in range(COL):    
+                if grid[r][c] == 1:
+                    cur_area = dfs(r, c, False)
+                    max_area = max(max_area, cur_area)
+                    
+        return max_area
+           
+
+'''
                 
         
         
