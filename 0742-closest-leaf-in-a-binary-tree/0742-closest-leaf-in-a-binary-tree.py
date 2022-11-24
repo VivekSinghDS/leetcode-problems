b592341e7d9a3410222a4da14224fde4e383829b
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-11-24 21:44:19 +0530

    Time: 81 ms (69.47%), Space: 15.2 MB (24.05%) - LeetHub

---
+++ 0742-closest-leaf-in-a-binary-tree/0742-closest-leaf-in-a-binary-tree.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findClosestLeaf(self, root: Optional[TreeNode], k: int) -> int:
+        def dfs(node, prev):
+            if not node:
+                return None 
+            
+            node.parent = prev
+            dfs(node.left, node)
+            dfs(node.right, node)
+            
+        def find(target):
+            queue = deque()
+            queue.append(root)
+            while queue:
+                for _ in range(len(queue)):
+                    node = queue.popleft()
+                    if node:
+                        if node.val == k:
+                            return node 
+                        queue.append(node.left)
+                        queue.append(node.right)
+                        
+                
+        dfs(root, None)
+        queue = deque()
+        target = find(k)
+        queue.append(target)
+        seen = set()
+        while queue:
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node:
+                    if not node.left and not node.right:
+                        return node.val
+                    for neighbor in [node.left, node.right, node.parent]:
+                        if neighbor and neighbor not in seen:
+                            seen.add(neighbor)
+                            queue.append(neighbor)
+                            if not neighbor.left and not neighbor.right:
+                                return neighbor.val
+                            
+        return root.val
+                    
+        
