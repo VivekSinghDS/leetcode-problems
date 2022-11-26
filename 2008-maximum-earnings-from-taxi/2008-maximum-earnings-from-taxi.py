9c694dd8c0eb4b36a1fc1c4a5effd5a57c0465ba
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-11-26 11:11:09 +0530

    Time: 4907 ms (27.63%), Space: 44.9 MB (34.48%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.py
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.py
@@ -1,20 +1,18 @@
 class Solution:
     def maxTaxiEarnings(self, n: int, rides: List[List[int]]) -> int:
-        rideInfo = defaultdict(list)
+        rideStart = defaultdict(list)
         
-        for start, end, tip in rides: # store the information about the rides
-            rideInfo[start].append((end, end - start + tip))
+        for start, end, tip in rides:
+            rideStart[start].append((end, end - start + tip))
             
-        max_cost_from_i = [0]*(n + 1) # the maximum amount we can get from ith position
+        # print(rideStart)
+        dp = [0]*(n+1)
         
-        for start in range(n - 1, 0, -1): # check from the last whether there is a start of some ride or not
-            for end, earned_amount in rideInfo[start]:
-                max_cost_from_i[start] = max(max_cost_from_i[start], max_cost_from_i[end] + earned_amount) 
-                # Above we have two options, one being take this ride into account and add from the end the remaining we can get
-                # The second choice is simply not include it. 
+        for i in range(n - 1, 0, -1):
+            for e, d in rideStart[i]:
+                dp[i] = max(dp[i], dp[e] + d)
                 
-            max_cost_from_i[start] = max(max_cost_from_i[start], max_cost_from_i[start + 1])
-            # Now, we see whether previously, have we computed the best value in start + 1 or not
+            dp[i] = max(dp[i], dp[i + 1])
             
-        return max_cost_from_i[1]
-        
+        # print(dp)
+        return dp[1]
