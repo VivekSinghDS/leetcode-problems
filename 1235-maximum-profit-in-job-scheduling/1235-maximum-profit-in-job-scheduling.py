8f73a1b97a2cc0a63cfe01626fbc2b1da0b79616
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-11-26 14:30:35 +0530

    Time: 1650 ms (30.02%), Space: 25.7 MB (90.46%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,6 +1,6 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        jobs = sorted(zip(startTime, endTime, profit))
+        jobs = sorted(zip(startTime, endTime, profit)) # combine all of them and sort 
         
         # print(jobs)
         
@@ -8,8 +8,7 @@ class Solution:
         dp = [0]*(n + 1)
         
         for i in reversed(range(n)):
-            k = bisect_left(jobs, jobs[i][1], key = lambda j : j[0])
-             # print(k)
+            k = bisect_left(jobs, jobs[i][1], key = lambda j : j[0]) # find the place where you can start the next schedule
             dp[i] = max(jobs[i][2] + dp[k], dp[i + 1])
             
         return dp[0]
