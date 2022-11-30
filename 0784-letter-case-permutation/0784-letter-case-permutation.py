71d36ca8c98d966e43ecbbf9270f6740f672234b
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-11-30 12:48:26 +0530

    Time: 154 ms (18.14%), Space: 15.5 MB (9.47%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.py
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -2,25 +2,25 @@ class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
         res = []
         
-        def backtrack(cur_comb, index):
+        def backtrack(cur_comb, i):
             nonlocal res
             if len(cur_comb) == len(s):
                 res.append("".join(cur_comb[:]))
                 return 
             
-            for i in range(index, len(s)):
-                if s[i].isdigit():
-                    backtrack(cur_comb + [s[i]], i + 1)
+            
+            if s[i].isdigit():
+                backtrack(cur_comb + [s[i]], i + 1)
                     
+            else:
+                if s[i].upper() == s[i]:
+                    backtrack(cur_comb + [s[i]], i + 1)
+                    backtrack(cur_comb + [s[i].lower()], i + 1)
+
                 else:
-                    if s[i].upper() == s[i]:
-                        backtrack(cur_comb + [s[i]], i + 1)
-                        backtrack(cur_comb + [s[i].lower()], i + 1)
-                        
-                    else:
-                        backtrack(cur_comb + [s[i].upper()], i + 1)
-                        backtrack(cur_comb + [s[i]], i + 1)
-                        
+                    backtrack(cur_comb + [s[i].upper()], i + 1)
+                    backtrack(cur_comb + [s[i]], i + 1)
+
                     
         backtrack([], 0)
         return res
