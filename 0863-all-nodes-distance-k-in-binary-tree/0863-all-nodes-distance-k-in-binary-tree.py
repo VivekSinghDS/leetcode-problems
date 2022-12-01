7a7c537cb36b664045cd7f8918684276a2c123d0
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-12-01 12:47:22 +0530

    Time: 35 ms (96.94%), Space: 14.6 MB (12.27%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -7,37 +7,35 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        def parenting_dfs(node, parent):
-            if node:
-                node.parent = parent 
-                parenting_dfs(node.left, node)
-                parenting_dfs(node.right, node)
-                
-        parenting_dfs(root, None)
+        def dfs(node, parent):
+            if not node:
+                return None 
+            
+            node.parent = parent
+            dfs(node.left, node)
+            dfs(node.right, node)
+            
+        dfs(root, None)
         
         queue = deque()
         queue.append((target, 0))
         seen = set()
-        seen.add(target)
+        res = []
         while queue:
             if queue[0][1] == k:
-                return [node.val for node, _ in queue]
+                return [node.val for node, distance in queue]
             
             for _ in range(len(queue)):
-                node, level = queue.popleft()
-                if node:
-                    if node.left and node.left not in seen:
-                        seen.add(node.left)
-                        queue.append((node.left, level + 1))
-                        
-                    if node.right and node.right not in seen:
-                        seen.add(node.right)
-                        queue.append((node.right, level + 1))
-                        
-                    if node.parent and node.parent not in seen:
-                        seen.add(node.parent)
-                        queue.append((node.parent, level + 1))
-                        
+                node, distance = queue.popleft()
+                if node not in seen:
+                    seen.add(node)
+                    
+                    for neighbor in [node.left, node.right, node.parent]:
+                        if neighbor and neighbor not in seen:
+                            queue.append((neighbor, distance + 1))
+                            
         return []
-                
-            
+                        
+                        
+                        
+        
