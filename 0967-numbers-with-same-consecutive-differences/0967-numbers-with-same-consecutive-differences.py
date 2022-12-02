b51f2cb012e65b08e9e11c4a4440e7bb9096aa6b
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-12-02 16:17:21 +0530

    Time: 138 ms (7.05%), Space: 14.1 MB (74.91%) - LeetHub

---
+++ 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
+        
+        res = []
+        
+        def backtrack(cur_comb):
+            if len(cur_comb) == n:
+                res.append("".join(cur_comb[:]))
+                return 
+            
+            for i in range(10):
+                if not cur_comb and i == 0:
+                    continue 
+                    
+                elif not cur_comb:
+                    backtrack(cur_comb + [str(i)])
+                    
+                else:
+                    if abs(int(cur_comb[-1]) - i) == k:
+                        backtrack(cur_comb + [str(i)])
+                    
+        backtrack([])
+        return res
+                
