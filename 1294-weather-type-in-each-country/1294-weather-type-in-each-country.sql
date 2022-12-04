a466035bfd98ff7fb57d7e7c9c1aa00f81a33297
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-12-04 14:23:58 +0530

    Time: 660 ms (61.18%), Space: 0B (100.00%) - LeetHub

--- 1294-weather-type-in-each-country/1294-weather-type-in-each-country.sql
+++ 1294-weather-type-in-each-country/1294-weather-type-in-each-country.sql
@@ -2,23 +2,27 @@
 with cte as (
 select country_id, weather_state, day
     from Weather where
-    EXTRACT(YEAR from day) = "2019" AND 
+    EXTRACT(YEAR from day) = "2019" AND  /* Extract DATE*/
     EXTRACT(MONTH from day) = "11" 
-), cte_2 as (
-    select country_id, AVG(weather_state) as "average_weather"
+), 
+
+cte_2 as (
+    select country_id, AVG(weather_state) as "average_weather" /* Get the Average */
     from cte 
     group by country_id
-), cte_3 as (
+), 
+
+cte_3 as (
     select c2.average_weather, c.country_name 
     from cte_2 c2 INNER JOIN Countries c 
-    ON c.country_id = c2.country_id
+    ON c.country_id = c2.country_id /* INNER JOIN with the countries table */
 )
 
 select country_name, 
 (
 CASE
 when (average_weather <= 15) THEN "Cold"
-when (average_weather >= 25) THEN "Hot"
+when (average_weather >= 25) THEN "Hot" /* get the result in the correct format */
 else "Warm"
 END
     ) as weather_type
