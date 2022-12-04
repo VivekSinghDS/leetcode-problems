0f3dd3e281e7ddb7199e2d13d74a6f60c3c10759
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-12-04 12:03:48 +0530

    Time: 2523 ms (47.96%), Space: 24.9 MB (80.12%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -1,17 +1,14 @@
 class Solution:
     def minimumAverageDifference(self, nums: List[int]) -> int:
-        sum_uptil_now = []
         running_sum = 0
         
         total = sum(nums)
-            
-        # print(sum_uptil_now)
+
         length = len(nums)
         res = float('inf')
         result = None
         for i in range(length):
             running_sum += nums[i]
-            # print(i + 1, length - i - 1, res)
             cur_length = i + 1
             first = int(running_sum / cur_length)
             if i == (length) - 1:
@@ -19,17 +16,13 @@ class Solution:
                 
             else:
                 second = int((total - running_sum) / (length - i - 1))
-            # print(first, second)
+
             ans = abs(first - second)
             
             if ans < res:
                 result = i
                 res = ans
-                
-            # print('-'*10)
-                
-            
-            
+
         return result
             
             
