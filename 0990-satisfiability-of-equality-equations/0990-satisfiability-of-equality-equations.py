9296c0a2bf95d0e42d4ad1d513e34b5c4d9bcaac
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-12-07 11:32:37 +0530

    Time: 93 ms (52.86%), Space: 14.2 MB (34.18%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
@@ -2,34 +2,37 @@ class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
         graph = defaultdict(list)
         
-        for relation in equations:
-            if "==" in relation:
-                first, second = relation.split('==')
-                graph[first].append(second)
-                graph[second].append(first)
-        
+        for equation in equations:
+            if "==" in equation:
+                equation = equation.split('==')
+                for a, b in zip(equation[0], equation[1]):
+                    graph[a].append(b)
+                    graph[b].append(a)
+                    
         # print(graph)
-        def dfs(node, target, visited):
+        def dfs(node, target, seen):
             if node == target:
                 return False 
             
-            visited.add(node)
+            elif node in seen:
+                return True
+            
             ans = True 
-            
+            seen.add(node)
             for neighbor in graph[node]:
-                if neighbor not in visited:
-                    ans &= dfs(neighbor, target, visited)
+                ans &= dfs(neighbor, target, seen)
                 
-            return ans
-            
-            
-        for relation in equations:
-            if "!=" in relation:
-                src, target = relation.split('!=')
-                visited = set()
-                if not dfs(src, target, visited):
-                    return False
+            return ans 
         
-        return True 
-
+        for equation in equations:
+            seen = set()
+            if "!=" in equation:
+                equation = equation.split('!=')
+                for a, b in zip(equation[0], equation[1]):
+                    if not dfs(a, b, seen):
+                        return False
+                    
+        return True
+                
+                    
         
