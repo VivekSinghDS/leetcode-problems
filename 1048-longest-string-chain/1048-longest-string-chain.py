05891fcdd7da672abf444ce77bf7b405b995c182
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-12-07 14:46:11 +0530

    Time: 143 ms (94.23%), Space: 18.1 MB (6.73%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        @cache
+        def dfs(word):            
+            res = 1
+            for i in range(len(word)):
+                new_word = word[:i] + word[i + 1:]
+                if new_word in hash_set:
+                    res = max(res, 1 + dfs(new_word))
+                    
+            return res
+                    
+            
+            
+        dp = {}
+        hash_set = set()
+        for word in words:
+            hash_set.add(word)
+            
+        ans = 0 
+        for word in words:
+            ans = max(ans, dfs(word))
+            
+        return ans 
+        
