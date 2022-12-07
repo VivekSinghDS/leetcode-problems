b780eb2a8d95b1bf9af6549961e1e33df66ee988
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-12-07 14:52:37 +0530

    Time: 139 ms (68.07%), Space: 13.9 MB (92.52%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        counter = Counter(tiles)
+        res = 0
+        
+        def backtrack(counter, cur_comb):
+            nonlocal res
+            if len(cur_comb) > 0:
+                res += 1
+                
+            for key in counter:
+                if counter[key] > 0:
+                    counter[key] -= 1
+                    backtrack(counter, cur_comb + key)
+                    counter[key] += 1
+                    
+        backtrack(counter, "")
+        return res
+
+        
+        
