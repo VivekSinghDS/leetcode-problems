43852be3ff9364f7ad01c214dea68e6e5380301d
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-12-17 15:42:49 +0530

    Time: 238 ms (96.12%), Space: 19.5 MB (16.28%) - LeetHub

---
+++ 1257-smallest-common-region/1257-smallest-common-region.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findSmallestRegion(self, regions: List[List[str]], region1: str, region2: str) -> str:
+        graph = {}
+        
+        for region in regions:
+            for area in region[1:]:
+                graph[area] = region[0]
+        
+        print(graph)
+        
+        r1_copy = region1
+        r2_copy = region2
+        
+        while region1 != region2:
+            
+            if graph.get(region1, False):
+                region1 = graph[region1]
+                
+            else:
+                region1 = r2_copy 
+                
+            if graph.get(region2, False):
+                region2 = graph[region2]
+                
+            else:
+                region2 = r1_copy
+                
+        return region1
+                
