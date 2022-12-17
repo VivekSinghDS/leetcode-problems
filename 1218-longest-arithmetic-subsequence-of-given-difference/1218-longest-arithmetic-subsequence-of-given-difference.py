15b9451f3713be28f296a0e2876319d2d72818c0
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-12-17 11:27:43 +0530

    Time: 544 ms (97.23%), Space: 27.7 MB (73.68%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.py
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.py
@@ -1,44 +1,11 @@
 class Solution:
-    def longestSubsequence(self, arr: List[int], difference: int) -> int:
-        '''
-        if len(arr) <= 2:
-            return len(arr)
+    def longestSubsequence(self, nums: List[int], difference: int) -> int:
+        dp = defaultdict(int)
         
-
-        def solve(i):
-            if i < 0:
-                return 0 
-            
-            ans = 0
-            for j in range(i - 1, -1, -1):
-                if arr[i] - arr[j] == difference:
-                    ans = max(ans, 1 + solve(j))
-                    
-            return ans
-        
-        res = 1
-        for i in range(len(arr)):
-            for j in range(i + 1, len(arr)):
-                if arr[j] - arr[i] == difference:
-                    res = max(res, 2 + solve(i))
-                
-        return res
-        
-        '''
-        
-        dp = {}
-        max_val = 1
-        for n in arr:
-            if n - difference in dp:
-                dp[n] = 1 + dp[n - difference]
-                
-            else:
-                dp[n] = 1
-                
-            # max_val = max(dp[n], max_val)
+        for n in nums:
+            dp[n] = 1 + dp[n - difference]
             
         return max(dp.values())
-                
             
         
                 
