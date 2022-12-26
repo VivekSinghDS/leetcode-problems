290c8372ee6e5dc0f818f588998af2b35ef60696
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-12-26 13:41:14 +0530

    Time: 6045 ms (22.53%), Space: 15.1 MB (57.68%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -3,17 +3,11 @@ class Solution:
         steps = [float('inf') for i in range(len(nums))]
         steps[0] = 0
         for i in range(len(nums)):
-            # print(steps)
             no_of_steps = nums[i]
-            j = i
-            # print(j, j + no_of_steps + 1)
-            while j < i + no_of_steps + 1 and j < len(nums):
-                # print(j)
+            
+            for j in range(i + 1, min(i + no_of_steps + 1, len(nums))):
                 steps[j] = min(steps[j], 1 + steps[i])
-                j += 1
-                
-            # print(steps)
-            # print('-'*10)
+
                 
         return steps[-1] 
                 
