c6fdd58c89a01112b32532897310afce9174f8ea
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-12-30 12:40:45 +0530

    Time: 117 ms (68.61%), Space: 15.7 MB (78.57%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,20 +1,18 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        target = len(graph) - 1
         res = []
-        
-        def backtrack(node, cur_comb):
-            nonlocal res
-            if node == len(graph) - 1:
-                res.append(cur_comb[:])
+        def dfs(node, cur_comb):
+            if node == target:
+                res.append(cur_comb + [node])
                 return 
             
-            for neighbor in graph[node]:
-                backtrack(neighbor, cur_comb + [neighbor])
-                
-        backtrack(0, [0])
+            for child in graph[node]:
+                if child not in seen:
+                    seen.add(child)
+                    dfs(child, cur_comb + [node])
+                    seen.remove(child)
+        
+        seen = set()
+        dfs(0, [])
         return res
-        
-                
-            
-            
-        
