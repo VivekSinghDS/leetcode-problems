7378bba062b3f6bb22989187aeee178ef0875ad9
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-12-30 12:41:56 +0530

    Time: 125 ms (66.77%), Space: 19.9 MB (6.92%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -2,17 +2,19 @@ class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         target = len(graph) - 1
         res = []
+        
+        @cache
         def dfs(node, cur_comb):
             if node == target:
-                res.append(cur_comb + [node])
+                res.append(cur_comb + (node,))
                 return 
             
             for child in graph[node]:
                 if child not in seen:
                     seen.add(child)
-                    dfs(child, cur_comb + [node])
+                    dfs(child, cur_comb + (node,))
                     seen.remove(child)
         
         seen = set()
-        dfs(0, [])
+        dfs(0, tuple())
         return res
