c16312888b8bcc87872cecc8bace9fb83578bd20
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-12-31 12:43:39 +0530

    Time: 67 ms (83.47%), Space: 14 MB (19.89%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -1,50 +1,51 @@
 class Solution:
     def uniquePathsIII(self, grid: List[List[int]]) -> int:
         start_row, start_col = 0, 0
-        end_row, end_col = 0, 0
-        empty = 0
+        end_row, end_col = 0, 0 
+        empty_squares = 0
         m = len(grid)
         n = len(grid[0])
-        res = 0
-        seen = set()
         
-        
-        for r in range(m):
-            for c in range(n):
-                if grid[r][c] == 1:
-                    start_row, start_col = r, c
-                    
-                elif grid[r][c] == 0:
-                    empty += 1
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 0:
+                    empty_squares += 1
                     
                 elif grid[r][c] == 2:
                     end_row, end_col = r, c
+                    
+                elif grid[r][c] == 1:
+                    start_row, start_col = r, c
+                    
+        # print(empty_squares)
         # print(start_row, start_col)
         # print(end_row, end_col)
-        # print(empty)
-        def dfs(r, c, seen, walk):
+        res = 0
+        def dfs(r, c, walk):
             nonlocal res
+            # print(r, c, walk)
             if (r == end_row and c == end_col):
-                if walk == empty + 1:
+                if (walk == empty_squares + 1 ):
                     res += 1
-                    
                 return 
-
             
             if(0 <= r < m and 0 <= c < n and grid[r][c] != -1 and (r, c) not in seen):
                 seen.add((r, c))
-                            
-                paths = [(r + 1, c),
-                        (r - 1, c),
-                        (r, c + 1),
-                        (r, c - 1)]
-                for x, y in paths:
-                    dfs(x, y, seen, walk + 1)
+                paths = [(r + 1, c), 
+                        (r - 1, c), 
+                        (r, c + 1), 
+                        (r, c - 1)
+                        ]
+            
+                for row, col in paths:
+                    
+                    dfs(row, col, walk + 1)
                     
                 seen.remove((r, c))
-                
-        dfs(start_row, start_col, seen, 0)
+                    
+            
+        seen = set()
+        dfs(start_row, start_col, 0)
         return res
-                    
-                    
+        
         
