1145b5c4e2200ec9438cd200ac45f4db8c504041
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-01-06 10:45:01 +0530

    Time: 244 ms (89.47%), Space: 28.7 MB (81.58%) - LeetHub

--- 1273-delete-tree-nodes/1273-delete-tree-nodes.py
+++ 1273-delete-tree-nodes/1273-delete-tree-nodes.py
@@ -1,54 +1,26 @@
 class Solution:
     def deleteTreeNodes(self, nodes: int, parent: List[int], value: List[int]) -> int:
         graph = defaultdict(list)
-        for node in range(nodes):
-            graph[parent[node]].append(node)
-
+        
+        for child, parent in enumerate(parent):
+            graph[parent].append(child)
+            
+            
+        # print(graph)
         def dfs(node):
             total_nodes = 1
             cur_sum = value[node]
             
-            for neighbor in graph[node]:
-                sum_count, nodes_count = dfs(neighbor)
+            for child in graph[node]:
+                children_sum, children_node_count = dfs(child)
                 
-                total_nodes += nodes_count
-                cur_sum += sum_count 
+                cur_sum += children_sum
+                total_nodes += children_node_count
                 
             if cur_sum == 0:
-                total_nodes = 0
-                
+                 total_nodes = 0 
+                    
             return (cur_sum, total_nodes)
         
         return dfs(0)[1]
-                
-            
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-
-        
-
         
