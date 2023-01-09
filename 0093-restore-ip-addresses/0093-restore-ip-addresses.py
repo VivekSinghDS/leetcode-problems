3e679367df07164c3fe7c3e16454d370cfbbfa43
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-01-09 17:33:51 +0530

    Time: 34 ms (92.85%), Space: 14.2 MB (6.09%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,30 +1,32 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
-        if not 4 <= len(s) <= 12:
+        if not(4 <= len(s) <= 12):
             return []
         
         res = []
-        def backtrack(index, curr):
+        
+        @cache
+        def backtrack(index, current_combination):
             nonlocal res
-            if len(curr) == 4:
+            current_combination = list(current_combination)
+            if len(current_combination) == 4:
                 if index == len(s):
-                    res.append(".".join(curr))
+                    res.append(".".join(current_combination[:]))
+                    
                 return 
             
-            num = ''
+            num = ""
             for i in range(index, min(index + 3, len(s))):
                 num += s[i]
                 
-                if num[0] == '0' and len(num) > 1:
+                if num[0] == "0" and len(num) > 1:
                     break 
                     
                 if 0 <= int(num) <= 255:
-                    curr.append(num)
-                    backtrack(i + 1, curr)
-                    curr.pop()
+                    backtrack(i + 1, tuple(current_combination + [num]))
                     
-        backtrack(0, [])
+        backtrack(0, tuple())
         return res
-
+                
             
         
