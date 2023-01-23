f1159066bf275aebaa304a3df60574a20389af3f
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-01-23 11:15:26 +0530

    Time: 987 ms (39.15%), Space: 19.3 MB (8.15%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        
+        for judgee, judges in trust:
+            graph[judgee].append(judges)
+            
+        flag = False 
+        judge = None
+        print(graph)
+        for key in range(1, n + 1):
+            if graph[key] == []:
+                judge = key
+                flag = True 
+        print(judge)       
+        if not flag:
+            return -1 
+        
+        flag = True 
+        for key in range(1, n + 1):
+            if key == judge:
+                continue
+            if judge not in graph[key]:
+                flag = False
+                
+        return -1 if not flag else judge
+                
+        
+        
+            
+        
