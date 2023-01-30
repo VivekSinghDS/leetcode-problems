84521536a021fea29d962e34bf7c8e57999e17e2
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-01-30 16:04:38 +0530

    Time: 863 ms (88.19%), Space: 27.9 MB (90.96%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -1,18 +1,16 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        heapify(costs)
         count = 0
-        while coins and costs:
-            ice_cream_cost = heapq.heappop(costs)
-            
-            if ice_cream_cost > coins:
-                break 
+        costs.sort()
+
+        for cost in costs:
+            if cost > coins:
+                break
                 
             else:
-                coins -= ice_cream_cost 
                 count += 1
+                coins -= cost
                 
         return count
-                
             
         
