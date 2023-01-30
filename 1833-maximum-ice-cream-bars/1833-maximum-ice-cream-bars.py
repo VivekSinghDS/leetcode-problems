20162e4c99c8c0c6c85675669a5b548611b0a13c
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-01-30 16:14:18 +0530

    Time: 843 ms (93.65%), Space: 27.9 MB (61.38%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -1,16 +1,25 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        count = 0
-        costs.sort()
+        n, icecreams = len(costs), 0
+        m = max(costs)
 
+        costsFrequency = [0] * (m + 1)
         for cost in costs:
-            if cost > coins:
+            costsFrequency[cost] += 1
+
+        for cost in range(1, m + 1):
+            # No ice cream is present costing 'cost'.
+            if not costsFrequency[cost]:
+                continue
+            # We don't have enough 'coins' to even pick one ice cream.
+            if coins < cost:
                 break
-                
-            else:
-                count += 1
-                coins -= cost
-                
-        return count
             
-        
+            # Count how many icecreams of 'cost' we can pick with our 'coins'.
+            # Either we can pick all ice creams of 'cost' or we will be limited by remaining 'coins'.
+            count = min(costsFrequency[cost], coins // cost)
+            # We reduce price of picked ice creams from our coins.
+            coins -= cost * count
+            icecreams += count
+            
+        return icecreams
