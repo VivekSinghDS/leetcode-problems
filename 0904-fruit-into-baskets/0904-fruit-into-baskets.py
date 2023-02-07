d335b99e284f8fcb89fe92230570d751f4f56a6f
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-02-07 13:30:41 +0530

    Time: 1077 ms (42.97%), Space: 19.5 MB (99.25%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,32 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        total = 0
+        mapper = {}
+        l = 0
+        count = 0
+        res = 0
+        
+        for r in range(len(fruits)):
+            mapper[fruits[r]] = 1 + mapper.get(fruits[r], 0)
+            
+            
+            if mapper[fruits[r]] == 1:
+                total += 1
+                
+            while total >= 3:
+                mapper[fruits[l]] -= 1
+                
+                if mapper[fruits[l]] == 0:
+                    mapper.pop(fruits[l])
+                    total -= 1
+                    
+                l += 1
+                    
+            res = max(res, sum(mapper.values()))
+        return res
+                    
+            
+                
+                
+        
+        
