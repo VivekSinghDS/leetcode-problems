5d8c83afd7f25f5e9f438ace286a58ca7316028f
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-02-08 14:24:04 +0530

    Time: 6159 ms (24.90%), Space: 15.1 MB (54.98%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,14 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-            jumps = 0
-            current_jump_end = 0
-            farthest = 0
-            for i in range(len(nums) - 1):
-                # we continuously find the how far we can reach in the current jump
-                farthest = max(farthest, i + nums[i])
-                # if we have come to the end of the current jump,
-                # we need to make another jump
-                if i == current_jump_end:
-                    jumps += 1
-                    current_jump_end = farthest
-            return jumps
+        steps = [float('inf') for i in range(len(nums))]
+        steps[0] = 0
+        
+        for i in range(len(nums)):
+            no_of_steps = nums[i]
+            
+            for j in range(i + 1, min(i + no_of_steps + 1, len(nums))):
+                steps[j] = min(steps[j], 1 + steps[i])
+        
+        print(steps)
+        return steps[-1]
