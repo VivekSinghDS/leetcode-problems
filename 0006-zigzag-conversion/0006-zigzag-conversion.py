84081536383f66b7d00ffd63cc160695fc83f4c7
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-02-10 10:04:53 +0530

    Time: 355 ms (12.35%), Space: 22.3 MB (5.01%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -2,23 +2,35 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1:
             return s
-
-        rows = [[] for _ in range(numRows)]
-        # print(rows)
-        row = 0
-        direction = 1
-
-        for c in s:
-            # print(rows)
-            rows[row].append(c)
-            row += direction
-
-            if row == 0 or row == numRows - 1:
-                direction *= -1
+        
+        n = len(s)
+        sections = ceil(n / (numRows + numRows - 2))
+        no_of_cols = sections * (numRows - 1)
+        
+        matrix = [[' '] * no_of_cols for i in range(numRows)]
+        cur_row, cur_col = 0, 0
+        cur_string_index = 0
+        
+        while cur_string_index < n:
+            while cur_row < numRows and cur_string_index < n:
+                matrix[cur_row][cur_col] = s[cur_string_index]
+                cur_string_index += 1
+                cur_row += 1
+            
+            cur_row -= 2
+            cur_col += 1
+            
+            while cur_row > 0 and cur_col < no_of_cols and cur_string_index < n:
+                matrix[cur_row][cur_col] = s[cur_string_index]
                 
-            # print('-'*10)
+                cur_row -= 1
+                cur_col += 1
+                cur_string_index += 1
                 
-        # print(rows)
-        ans = ["".join(row) for row in rows]
-        return "".join(ans)
+        # print(matrix)
+        answer = ""
+        for row in matrix:
+            answer += "".join(row)
+            
+        return(answer.replace(" ", ""))
         
