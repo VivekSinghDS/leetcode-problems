0cf7e28e24ae58d908a05d14077842c78a49e36f
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-02-11 10:04:19 +0530

    Time: 101 ms (34.16%), Space: 14.3 MB (31.23%) - LeetHub

---
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -0,0 +1,37 @@
+class Solution:
+    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+        graph = defaultdict(set)
+        
+        for src, tar in redEdges:
+            graph[src].add((tar, "red"))
+            
+        for src, tar in blueEdges:
+            graph[src].add((tar, "blue"))
+            
+        # print(graph)
+        res = [float('inf')]*n
+        
+        queue = deque()
+        queue.append((0, 0, None))
+        distance = 0
+        seen = set()
+        while queue:
+            node, distance, pre = queue.popleft()
+            res[node] = min(res[node], distance)
+            if not pre:
+                for neighbor, color in graph[node]:
+                    if (neighbor, color) not in seen:
+                        seen.add((node, neighbor, color))
+                        queue.append((neighbor, 1 + distance, color))
+                    
+            else:
+                for neighbor, color in graph[node]:
+                    if color != pre and (neighbor, color) not in seen:
+                        seen.add((neighbor, color))
+                        queue.append((neighbor, 1 + distance, color))
+        
+        
+        return [-1 if r == float('inf') else r for r in res] 
+            
+            
+            
