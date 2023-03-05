2ba453593eac7de03702eeec9c3dcf001b760efd
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-03-05 16:59:06 +0530

    Time: 777 ms (76.15%), Space: 27.8 MB (13.81%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,10 +1,15 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        output = []
+        mapper = defaultdict(int)
         
-        heapify(nums)
-
-        while nums:
-            output.append(heapq.heappop(nums))
-            
+        for n in nums:
+            mapper[n] += 1
+        
+        min_val = min(mapper.keys())
+        max_val = max(mapper.keys())
+        output = []
+        for key in range(min_val, max_val + 1):
+            if key in mapper:
+                output.extend([key]*mapper[key])
+                
         return output
