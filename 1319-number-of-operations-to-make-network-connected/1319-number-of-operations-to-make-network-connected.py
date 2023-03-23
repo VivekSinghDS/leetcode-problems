42fd40fca282ac1097ce7753c526a6c09da527ef
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-03-23 17:44:20 +0530

    Time: 534 ms (44.17%), Space: 49.2 MB (10.34%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -0,0 +1,35 @@
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        if len(connections) < n - 1:
+            return -1
+        
+        graph = defaultdict(list)
+        for source, target in connections:
+            graph[source].append(target)
+            graph[target].append(source)
+            
+        res = []
+        def dfs(node):
+            nonlocal count
+            if node not in seen:
+                # print(node, seen)
+                count += 1
+                seen.add(node)
+                
+                for neighbor in graph[node]:
+                    dfs(neighbor)
+
+        seen = set()
+        ans = 0
+        for i in range(n):
+            if i not in seen:
+                count = 0
+                dfs(i)
+                if i > 0:
+                    ans += 1
+                
+                
+        return ans
+
+                
+        
