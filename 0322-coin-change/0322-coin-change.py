9e5a1686804aeff8483e1efd92a69a165c77e856
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-04-15 16:06:37 +0530

    Time: 1458 ms (63.51%), Space: 35.5 MB (12.75%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,54 +1,21 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        '''
-        res = float('inf')
-        coins.sort(reverse = True)
-        
-        @lru_cache(maxsize = None)
-        def backtrack(cur_length, remain):
-            nonlocal res
-            if remain < 0 or cur_length > res:
-                return 
-            
-            if remain == 0:
-                res = min(res, cur_length)
-                return 
-            
-            for c in coins:
-                remain -= c 
-                cur_length += 1
-                
-                backtrack(cur_length, remain)
-                
-                remain += c
-                cur_length -= 1
-                
-        backtrack(0, amount)
-        return res if res != float('inf') else -1
-        '''
-        @lru_cache(None)
-        def dfs(rem):
-            if rem == 0:
-                return 0
-            
-            if rem < 0:
+        @cache
+        def dfs(remaining):
+            if remaining < 0:
                 return -1
             
+            elif remaining == 0:
+                return 0 
+            
             min_cost = float('inf')
-            for c in coins:
-                res = dfs(rem - c)
+            for coin in coins:
+                res = dfs(remaining - coin)
                 if res != -1:
-                    min_cost = min(min_cost, res + 1)
+                    min_cost = min(min_cost, 1 + res)
                     
-            return min_cost if min_cost != float('inf') else -1
+            return min_cost
         
-        return dfs(amount)
-        
-        
+        return dfs(amount) if dfs(amount) != float('inf') else -1
                 
-        
-            
-            
-            
-            
         
