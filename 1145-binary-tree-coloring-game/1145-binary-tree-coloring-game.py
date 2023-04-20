680c8e8a03e85b2345c3c94d8d3f895c2175353c
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-04-20 12:47:58 +0530

    Time: 37 ms (47.79%), Space: 13.9 MB (79.41%) - LeetHub

--- 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.py
+++ 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.py
@@ -34,28 +34,20 @@ class Solution:
             right = dfs(node.right)                
             return 1 + left + right
         
-        if x == root.val:
-            left_size = dfs(root.left)
-            right_size = dfs(root.right)
-            
-            if left_size == right_size:
-                return False
-            else:
-                return True
+        
+        x_node = get_x(root)
+
+        left_count = dfs(x_node.left)
+        right_count = dfs(x_node.right)
+        # print(left_count, right_count)
+        parent_count = n - left_count - right_count - 1
+        if (parent_count > left_count + right_count or 
+            left_count > parent_count + right_count or 
+            right_count > parent_count + left_count):
+            return True 
+
         else:
-            x_node = get_x(root)
-            
-            left_count = dfs(x_node.left)
-            right_count = dfs(x_node.right)
-            # print(left_count, right_count)
-            parent_count = n - left_count - right_count - 1
-            if (parent_count > left_count + right_count or 
-                left_count > parent_count + right_count or 
-                right_count > parent_count + left_count):
-                return True 
-            
-            else:
-                return False
+            return False
             
             
             
