04f3d1067f0dad3a945ebd651f0a33a180198e55
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-05-06 12:29:42 +0530

    Time: 610 ms (95.33%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,3 +1,10 @@
-Select (a.id) as Id from weather a, weather b
-where
-datediff(a.recordDate , b.recordDate)=1 and a.temperature > b.temperature ;
+# Write your MySQL query statement below
+with cte as (
+    select id, 
+    lag(temperature) over(order by recordDate) as "previous_temperature",
+    lag(recordDate) over(order by recordDate) as "previous_date", temperature, recordDate
+    from  Weather
+)
+
+select id from cte
+where temperature - previous_temperature > 0 and DATEDIFF(recordDate, previous_date) = 1
