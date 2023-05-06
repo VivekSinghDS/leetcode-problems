3502811f9fc8703115639cf587bf3831ce5224c8
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-05-06 11:30:35 +0530

    Time: 2295 ms (38.73%), Space: 0B (100.00%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,3 +1,8 @@
 # Write your MySQL query statement below
-select p.product_name, s.year, s.price from 
-Sales s INNER JOIN Product p ON p.product_id = s.product_id
+with cte as (
+    select p.product_name, s.year, s.price
+    from Product p INNER JOIN Sales s ON p.product_id = s.product_id
+    
+)
+
+select * from cte
