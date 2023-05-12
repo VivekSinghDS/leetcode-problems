e0383f8a46496397e7d0da35485c71842e605ea3
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-05-12 15:44:50 +0530

    Time: 80 ms (49.02%), Space: 16.6 MB (40.46%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
@@ -1,30 +1,19 @@
 class Solution:
     def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
-        def compress_matrix(matrix: List[List[int]]) -> List[List[int]]:
-            rows, cols = len(matrix), len(matrix[0])
-            compressed_matrix = [[] for _ in range(rows)]
-            for row in range(rows):
-                for col in range(cols):
-                    if matrix[row][col]:
-                        compressed_matrix[row].append([matrix[row][col], col])
-            return compressed_matrix
-        
-        m = len(mat1)
-        k = len(mat1[0])
-        n = len(mat2[0])
-        
-        # Store the non-zero values of each matrix.
-        A = compress_matrix(mat1)
-        B = compress_matrix(mat2)
-        
-        ans = [[0] * n for _ in range(m)]
-        
-        for mat1_row in range(m):
-            # Iterate on all current 'row' non-zero elements of mat1.
-            for element1, mat1_col in A[mat1_row]:
-                # Multiply and add all non-zero elements of mat2
-                # where the row is equal to col of current element of mat1.
-                for element2, mat2_col in B[mat1_col]:
-                    ans[mat1_row][mat2_col] += element1 * element2
-                    
-        return ans
+        m, nA, n = len(mat1), len(mat1[0]), len(mat2[0])
+        res = [[0]*n for _ in range(m)]
+        rows = []
+        for i in range(m):
+            row = []
+            for j in range(nA):
+                if mat1[i][j]!=0:
+                    row.append(j)
+                    row.append(mat1[i][j])
+            rows.append(row)
+
+        for i in range(m):
+            row  = rows[i]
+            for j in range(0, len(row), 2):
+                for k in range(n):
+                    res[i][k] += mat2[row[j]][k] * row[j+1]
+        return res
