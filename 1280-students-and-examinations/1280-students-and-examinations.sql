7c8f96e858c9c67955ede7d51625973efbbf58ce
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-05-13 15:56:35 +0530

    Time: 1438 ms (87.98%), Space: 0B (100.00%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,22 +1,15 @@
 # Write your MySQL query statement below
 with cte as (
     select * from Students JOIN Subjects
-), 
-
-cte_2 as (
+), cte_2 as (
     select student_id, subject_name, 
-    count(*) as "attended_exams"
-    from Examinations 
+    count(*) as "attended_count" from Examinations 
     group by student_id, subject_name
-), 
-
-cte_3 as (
-    select c.student_id, c.student_name, c.subject_name, 
-    case when c2.attended_exams is not null then c2.attended_exams else 0 end as "attended_exams"
-    from cte c LEFT JOIN cte_2 c2 ON 
-    c.student_id = c2.student_id and c.subject_name = c2.subject_name
+), cte_3 as (
+    select c.student_id, c.student_name, c.subject_name, IF(c2.attended_count is NULL, 0, c2.attended_count) as "attended_exams"
+    from cte c LEFT JOIN cte_2 c2 ON c.student_id = c2.student_id AND c.subject_name = c2.subject_name
     order by student_id, subject_name
+
 )
 
 select * from cte_3
-
