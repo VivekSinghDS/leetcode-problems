c115db1177292e10f6f7f4a4825170b2de70f383
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-05-13 12:03:55 +0530

    Time: 365 ms (44.88%), Space: 182.7 MB (37.01%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
@@ -1,49 +1,17 @@
 class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
-        '''
-        # simple top-down approach 
-        counter = {"0" : zero, "1" : one}
-        
-        @cache
-        def backtrack(current_string):
-            if len(current_string) > high:
-                return 0
-            
-            total = 0
-            if low <= len(current_string) <= high:
-                total += 1% (10 ** 9 + 7)
-                
-            for key in counter:
-                total += backtrack(current_string + key * counter[key])% (10 ** 9 + 7)
-                
-            return total % (10 ** 9 + 7)
-                
-        return backtrack("") 
-
-        '''
-        dp = defaultdict(int)
         mod = 10 ** 9 + 7
-        
-        dp[0] = 1
-        
-        for i in range(1, high + 1):
-            if i - zero >= 0:
-                dp[i] += dp[i - zero]
-                
-            if i - one >= 0:
-                dp[i] += dp[i - one]
-                
-            dp[i] %= mod
-        ans = 0   
-        for i in range(low, high + 1):
-            ans += dp[i]
+        @cache
+        def dfs(length):
+            if length == high:
+                return 1
             
-        return ans % mod
+            elif length > high:
+                return 0 
             
-        
-        
-        
-        
-                
-                
+            count = 1 if length >= low else 0
+            count += dfs(length + zero) % mod
+            count += dfs(length + one) % mod
+            return count % mod
+        return dfs(0) % mod
         
