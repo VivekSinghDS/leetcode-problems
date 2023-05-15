c65ecc88b0112d6fe40452248290a5f228d2af21
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-05-15 14:39:49 +0530

    Time: 120 ms (65.00%), Space: 16.3 MB (30.96%) - LeetHub

--- 0265-paint-house-ii/0265-paint-house-ii.py
+++ 0265-paint-house-ii/0265-paint-house-ii.py
@@ -1,21 +1,25 @@
 class Solution:
     def minCostII(self, costs: List[List[int]]) -> int:
-        
-        
-        @cache
-        def dfs(index, previous):
-            if index >= len(costs):
-                return 0
     
-            cost = float('inf')
-            for i in range(len(costs[index])):
-                if i == previous:
-                    continue 
-                cost = min(cost, costs[index][i] + dfs(index + 1, i))
-            return cost 
+        n = len(costs)
+        k = len(costs[0])
         
-        return dfs(0, None)
-                
-                
-                
-        
+        for house in range(1, n):
+            min_color, second_min = None, None 
+            
+            for color in range(k):
+                cost = costs[house-1][color]
+                if min_color is None or cost < costs[house - 1][min_color]:
+                    second_min = min_color 
+                    min_color = color
+                    
+                elif second_min is None or cost < costs[house - 1][second_min]:
+                    second_min = color
+                    
+            for color in range(k):
+                if color == min_color:
+                    costs[house][color] += costs[house - 1][second_min]
+                else:
+                    costs[house][color] += costs[house - 1][min_color]
+                    
+        return (min(costs[-1]))
