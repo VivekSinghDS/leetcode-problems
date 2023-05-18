ff561e9d657b374953a0602daa124e25c6788f52
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-05-18 11:32:03 +0530

    Time: 1284 ms (13.25%), Space: 54.7 MB (29.97%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
@@ -1,7 +1,6 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
-        indegree_nodes = []
         i_set = set()
         
         for source, target in edges:
@@ -10,23 +9,28 @@ class Solution:
         for i in range(n):
             for nodes in graph[i]:
                 i_set.add(nodes)
-        
-        @cache
-        def dfs(node):
-            if node in seen:
-                return 
-            
-            seen.add(node)
-            for neighbor in graph[node]:
-                dfs(neighbor)
-                
-        seen = set()
-        res = []
         # print(i_set)
+        res = []
         for i in range(n):
-            if i not in i_set and i not in seen:
+            if i not in i_set:
                 res.append(i)
-                dfs(i)
-                # print(seen)
-                
         return res
+#         @cache
+#         def dfs(node):
+#             if node in seen:
+#                 return 
+            
+#             seen.add(node)
+#             for neighbor in graph[node]:
+#                 dfs(neighbor)
+                
+#         seen = set()
+#         res = []
+#         # print(i_set)
+#         for i in range(n):
+#             if i not in i_set and i not in seen:
+#                 res.append(i)
+#                 dfs(i)
+#                 # print(seen)
+                
+        
