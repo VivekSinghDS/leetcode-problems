a27967a344c112a04be865eed0f192a301893bba
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-05-21 09:55:15 +0530

    Time: 2055 ms (75.00%), Space: 376.6 MB (25.00%) - LeetHub

--- 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.py
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.py
@@ -1,6 +1,8 @@
 class Solution:
     def punishmentNumber(self, n: int) -> int:
-        def dfs(index, current_sum):
+        
+        @cache
+        def dfs(index, current_sum, number):
             nonlocal res
             if index >= len(number):
                 if current_sum == target:
@@ -12,17 +14,16 @@ class Solution:
                 return 
             
             
-            
             for j in range(index + 1, len(number) + 1):
                 new_sum = int(number[index : j])
-                dfs(j, current_sum + new_sum)
+                dfs(j, current_sum + new_sum, number)
                 
         ans = 1
         for i in range(2, n + 1):
             number = str(i ** 2)
             target = i
             res = False
-            dfs(0, 0)
+            dfs(0, 0, number)
             
             if res:
                 ans += int(number)
