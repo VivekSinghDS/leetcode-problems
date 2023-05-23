c4e35151d6c85c4337d31bf902133d9cdbcd5ce5
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-05-23 15:24:03 +0530

    Time: 1322 ms (64.65%), Space: 40.8 MB (12.16%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
@@ -1,30 +1,15 @@
 class Solution:
     def maxMoves(self, grid: List[List[int]]) -> int:
-        def invalid(r, c):
-            return r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0])
+        m, n, dirs = len(grid), len(grid[0]), [(0, 1), (1, 1), (-1, 1)]
         @cache
-        def dfs(r, c):
-            if invalid(r, c):
-                return 0
-            
-            count = 1 
-            moves = 0
-            paths = [(r - 1, c + 1), (r, c + 1), (r + 1, c + 1)]
-            for row, col in paths:
-                if not invalid(row, col) and grid[row][col] > grid[r][c] and (row, col) not in seen:
-                    seen.add((row, col))
-                    moves = max(moves, dfs(row, col))
-                    seen.remove((row, col))
-            
-            return count + moves
-        
-        
-        res = 0
-        seen = set()
-        for i in range(len(grid)):
-            
-            res = max(res, dfs(i, 0) - 1)
-        return res
+        def dp(i, j):
+            ans = 0
+            for x, y in dirs:
+                ni, nj = i + x, j + y
+                if 0 <= ni < m and nj < n and grid[i][j] < grid[ni][nj]:
+                    ans = max(ans, 1 + dp(ni, nj))
+            return ans
+        return max(dp(i, 0) for i in range(m))
                 
                 
             
