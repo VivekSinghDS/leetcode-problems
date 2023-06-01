023b149ac0616d4e4d9e829ddace8947eb23bb69
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-06-01 15:34:17 +0530

    Time: 215 ms (65.75%), Space: 17.4 MB (69.29%) - LeetHub

---
+++ 2712-minimum-cost-to-make-all-characters-equal/2712-minimum-cost-to-make-all-characters-equal.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumCost(self, s: str) -> int:
+        
+        ans = 0 
+        
+        for i in range(1, len(s)):
+            if s[i] != s[i - 1]:
+                ans += min(i, len(s) - i)
+        return ans
+            
+                
+        
