cb171c2063883eac96f1c7c0bf127aec52713589
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-06-01 11:50:10 +0530

    Time: 900 ms (11.31%), Space: 18 MB (12.33%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,42 +1,34 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         queue = deque()
-        
-        if grid[0][0] == 1:
-            return -1
-        
+        res = float('inf')
         queue.append((0, 0, 1))
-        shortest = float('inf')
         seen = set()
-        
         while queue:
             for _ in range(len(queue)):
+                row, col, count = queue.popleft()
                 
-                r, c, count = queue.popleft()
-                if r == len(grid) - 1 and c == len(grid[0]) - 1:
-                    shortest = min(shortest, count)
+                if (not(0 <= row < len(grid)) 
+                   or not(0 <= col < len(grid[0])) or (grid[row][col] == 1)) or (row, col) in seen:
                     
-                elif (r, c) not in seen:
-                    seen.add((r, c))
-                    paths = [(r + 1, c),
-                            (r - 1, c),
-                            (r, c + 1),
-                            (r, c - 1),
-                             
-                            (r + 1, c + 1),
-                            (r + 1, c - 1),
-                            (r - 1, c - 1), 
-                            (r - 1, c + 1)]
+                    continue
+                # print(row, col)
+                seen.add((row, col))
+                if row == len(grid) - 1 and col == len(grid[0]) - 1:
+                    res = min(res, count)
                     
-                    for row, col in paths:
-                        if( 0 <= row < len(grid) and 0 <= col < len(grid[0]) and 
-                          grid[row][col] == 0):
-                            queue.append((row, col, count + 1))
-                            
-        return shortest if shortest != float('inf') else -1
-            
-        
-        
-            
-                        
+                path = [(row + 1, col), (row - 1, col), 
+                       (row, col + 1), (row, col - 1), 
+                       (row + 1, col + 1), (row - 1, col - 1), 
+                       (row - 1, col + 1), (row + 1, col - 1)]
+                
+                for r, c in path:
+                    queue.append((r, c, count + 1))
+        return res if res != float('inf') else -1
+                
+                    
+                
+                    
+                
+                
         
