adeb88508af091ab1531b0fc8b8bdb1969c673bf
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-06-03 12:20:29 +0530

    Time: 1288 ms (71.53%), Space: 54.1 MB (24.25%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        graph = defaultdict(list)
+        for i in range(len(manager)):
+            graph[manager[i]].append(i)
+            
+        # print(graph)
+        
+        def dfs(manager):
+            time = informTime[manager]
+            child_count = 0
+            for neighbor in graph[manager]:
+                child_count = max(child_count, dfs(neighbor))
+                
+                
+            return time + child_count
+        
+        return dfs(headID)
+            
+        
