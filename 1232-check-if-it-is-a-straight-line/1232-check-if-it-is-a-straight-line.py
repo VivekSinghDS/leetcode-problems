52c0641dea0d158a9353c34c38b5d4b095856269
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-06-05 11:05:34 +0530

    Time: 88 ms (5.24%), Space: 16.9 MB (27.29%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
@@ -1,6 +1,6 @@
 class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        coordinates.sort(key = lambda x:x[0])
+        # coordinates.sort(key = lambda x:x[0])
         slope = None
         for i in range(1, len(coordinates)):
             y2, x2 = coordinates[i - 1][1], coordinates[i - 1][0]
