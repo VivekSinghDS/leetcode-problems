f45320e8cb468e153cae12e5767c3d6410d6fb07
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-06-07 11:46:00 +0530

    Time: 52 ms (15.38%), Space: 16.4 MB (25.27%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.py
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -1,39 +1,12 @@
 class Solution:
     def minFlips(self, a: int, b: int, c: int) -> int:
-        a = str(bin(a))
-        b = str(bin(b))
-        c = str(bin(c))
-        # print(a, b, c)
-        a = a.split('0b')[1]
-        b = b.split('0b')[1]
-        c = c.split('0b')[1]
-        
-        if len(a) > len(b):
-            b = "0"*(len(a) - len(b)) + b
-        else:
-            a = "0" *(len(b) - len(a)) + a
-        
-        if len(c) < len(a):
-            c = "0"*(len(a) - len(c)) + c
-        count = 0
-        if len(c) > len(a):
-            a = "0"*(len(c) - len(a)) + a
-            b = "0"*(len(c) - len(b)) + b
-        # print(a, b, c)
-        for i, j, ans in zip(a[::-1], b[::-1], c[::-1]):
-            if ans == "1":
-                if str(1) in [i, j]:
-                    continue 
-                else:
-                    # print('for c = ', ans)
-                    count += 1
-            
-            elif ans == "0":
-                # print('here')
-                count += int(i) + int(j)
-        return count
-                
-            
-            
-        
-        
+        answer = 0
+        while a or b or c:
+            if c & 1:
+                answer += 0 if ((a & 1) or (b & 1)) else 1
+            else:
+                answer += (a & 1) + (b & 1)
+            a >>= 1
+            b >>= 1
+            c >>= 1
+        return answer
