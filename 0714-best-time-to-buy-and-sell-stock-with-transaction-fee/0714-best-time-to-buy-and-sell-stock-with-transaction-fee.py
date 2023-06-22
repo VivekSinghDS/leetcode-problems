9beb5985ca05fa9a0bf54d4d088f3f05d33d913e
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-06-22 14:47:20 +0530

    Time: 1419 ms (16.80%), Space: 257.1 MB (11.68%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,43 +1,15 @@
-'''
-class Solution:
-    def maxProfit(self, prices: List[int], fee: int) -> int:        
-        @cache
-        def dfs(index, previous):
-            if index >= len(prices):
-                return 0 
-            
-            profit = float('-inf')
-            include = float('-inf')
-            exclude = float('-inf')
-            
-            if previous is None:
-                include = dfs(index + 1, prices[index])
-                exclude = dfs(index + 1, None)
-                
-            elif previous > prices[index]:
-                exclude = dfs(index + 1, previous)
-                
-            else:
-                value = prices[index] - previous - fee 
-                if value > 0:
-                    include = value + dfs(index + 1, None)
-                exclude = dfs(index + 1, previous)
-            profit = max(profit, include, exclude)
-            return profit
-        return max(dfs(0, prices[0]), dfs(0, None))
-                
-'''
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        n = len(prices)
-        hold, free = [0] * n, [0] * n
-        
-        # In order to hold a stock on day 0, we have no other choice but to buy it for prices[0].
-        hold[0] = -prices[0]
-        
-        for i in range(1, n):
-            hold[i] = max(hold[i - 1], free[i - 1] - prices[i])
-            free[i] = max(free[i - 1], hold[i - 1] + prices[i] - fee)
-        
-        return free[-1]
-                    
+        # 2023/06/21 21:36PM -> 21:44PM
+        @cache
+        def dfs(i, hold):
+            if i >= len(prices):
+                return 0
+            # recursive
+            if not hold:
+                # buy stock / skip stock
+                return max(-prices[i] + dfs(i + 1, not hold), dfs(i + 1, hold))
+            else:
+                # hold stock / sell stock
+                return max(dfs(i + 1, hold), prices[i] + dfs(i + 1, not hold) - fee)
+        return dfs(0, False)
