b562093ddeddd480bdc53acbe21a3f3f35e0e4ea
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-06-23 09:58:19 +0530

    Time: 6482 ms (5.10%), Space: 99.9 MB (6.40%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.py
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.py
@@ -1,38 +1,10 @@
 class Solution:
     def longestArithSeqLength(self, nums: List[int]) -> int:
-        '''
-        res = 1
+        mapper = defaultdict(int)
         
-        dp = {}
-        def solve(i, diff):
-            if (i, diff) in dp:
-                return dp[(i, diff)]
-            if i < 0:
-                return 0
-            
-            ans = 0
-            for j in range(i - 1, -1, -1):
-                if nums[i] - nums[j] == diff:
-                    ans = max(ans, 1 + solve(j, diff))
-            
-            dp[(i, diff)] = ans
-            return ans
-                
-        
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                diff = nums[j] - nums[i]
-                res = max(res, 2 + solve(i, diff))
-                
-        return res
-        
-        '''
-        mapper = {}
-        
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                mapper[j, nums[j] - nums[i]] = mapper.get((i, nums[j] - nums[i]), 1) + 1
-                
-        return max(mapper.values())
-            
+        for right in range(len(nums)):
+            for left in range(right):
+                difference = nums[right] - nums[left]
+                mapper[(right, difference)] = max(mapper[(right, difference)], 1 + mapper[(left, difference)])
+        return max(mapper.values()) + 1
         
