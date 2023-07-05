f0267b210f89c1acbc5f6339182f3080f5a26807
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-07-05 15:32:24 +0530

    Time: 385 ms (58.83%), Space: 19.2 MB (11.96%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -6,7 +6,7 @@ class Solution:
         if 0 not in nums:
             return len(nums) - 1
         for r in range(len(nums)):
-            # print(nums[l: r + 1])
+
             if nums[r] == 0:
                 zero_count += 1
                 
@@ -14,8 +14,7 @@ class Solution:
                 if nums[l] == 0:
                     zero_count -= 1
                 l += 1
-            
-            # print(nums[l: r + 1])
+        
             res = max(res, r - l + 1 if zero_count == 0 else r - l)
                     
         return res
