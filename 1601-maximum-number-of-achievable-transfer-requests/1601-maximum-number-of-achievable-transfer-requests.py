4b681b5e7c97c41e79f2c21dc1b85c36ef1a18c5
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2023-07-13 11:37:57 +0530

    Time: 1167 ms (65.84%), Space: 16.5 MB (29.64%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
@@ -1,26 +1,25 @@
 class Solution:
     def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
-        res = 0
-        @cache
-        def dfs(index, balance):
-            nonlocal res
-            if index == len(requests):
-                count = 0
-                for b in balance:
-                    if b == 0:
-                        count += 1
-                    else:
-                        return float('-inf')
-                else:
-                    return 0
-            
-            balance_2 = list(balance)
-            balance_2[requests[index][0]] -= 1
-            balance_2[requests[index][1]] += 1
-            
-            return max(1 + dfs(index + 1, tuple(balance_2)), dfs(index + 1, balance))
-        return dfs(0, tuple([0]*n))
-                
-                    
-                    
         
+      table = [0] * n
+      ans = 0
+      m = len(requests)
+
+      def bt(index, selected):
+        nonlocal ans
+        if index == m:
+          if max(table) == 0:
+            ans = max(ans, selected)
+          return
+
+        f, t = requests[index]
+        # if select
+        table[f] -= 1
+        table[t] += 1
+        bt(index + 1, selected + 1)
+        table[f] += 1
+        table[t] -= 1
+        # if not select
+        bt(index + 1, selected)
+      bt(0, 0)
+      return ans
